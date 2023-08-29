9996016ce087f2924699935d418becec3eb2ea2f
Dark-jin <chch4lee1@naver.com>
2023-08-29 21:50:34 +0900

    add : css-loader, style-loader, webpack.config devServer static

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 // .env
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 // 웹팩에서 html을 파싱하기 위함.
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // tsconfig에서 baseUrl을 받아오기 위함
-const TsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 // gzip
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 // 번들 점유 용량 확인
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 // Typescript(타입스크립트)를 빌드할 때 성능을 향상시키기 위한 플러그인를 불러오기
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 // uglify
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 dotenv.config();
 
 module.exports = {
   entry: {
     // 번들 파일(bundle)의 시작 파일(Entry)을 jsx에서 tsx로 변경
-    app: "./src/index.tsx",
+    app: './src/index.tsx'
   },
   output: {
-    path: path.join(__dirname, "/dist"),
-    publicPath: "/",
-    filename: "build.[chunkhash].js",
-    clean : true, // 빌드 이전 결과물 제거
-    assetModuleFilename: "assets/[name][ext]", // asset 폴더에 있던 파일들은 dist 내부에 asset 폴더 생성후 이름과 확장자를 그대로 사용하여 저장
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'build.[chunkhash].js',
+    clean: true, // 빌드 이전 결과물 제거
+    assetModuleFilename: 'assets/[name][ext]' // asset 폴더에 있던 파일들은 dist 내부에 asset 폴더 생성후 이름과 확장자를 그대로 사용하여 저장
   },
   resolve: {
     plugins: [new TsConfigPathsPlugin()],
-    extensions: [".ts", ".tsx", ".js", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.json']
   },
   module: {
     rules: [
@@ -47,26 +47,26 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              plugins: ["@babel/transform-runtime"],
-            },
+              plugins: ['@babel/transform-runtime']
+            }
           },
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
-              transpileOnly: true,
-            },
-          },
+              transpileOnly: true
+            }
+          }
         ],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       // css loader 설정
       {
         test: /\.(s*)css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   // proxy 설정
   devServer: {
@@ -74,35 +74,38 @@ module.exports = {
     // proxy: {
     //   "/api": "http://localhost:5000",
     // },
-    port : 3000,
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000
   },
   // uglify
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin()]
   },
   plugins: [
     // 기본 html 위치 설정.
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
+      template: './public/index.html',
+      filename: 'index.html'
     }),
     // Typescript(타입스크립트)의 컴파일 속도 향상을 위한 플러그인을 설정
     new ForkTsCheckerWebpackPlugin(),
     // new BundleAnalyzerPlugin(),
     // gzip (압축)
     new CompressionPlugin({
-      algorithm: "gzip",
+      algorithm: 'gzip'
     }),
 
     // env 변수 사용을 위한 플러그인
     new webpack.DefinePlugin({
-      "process.env.REACT_APP_S3_ACCESS_KEY": JSON.stringify(
-        process.env.REACT_APP_S3_ACCESS_KEY,
+      'process.env.REACT_APP_S3_ACCESS_KEY': JSON.stringify(
+        process.env.REACT_APP_S3_ACCESS_KEY
       ),
-      "process.env.REACT_APP_S3_SECRET_ACCESS_KEY": JSON.stringify(
-        process.env.REACT_APP_S3_SECRET_ACCESS_KEY,
-      ),
-    }),
-  ],
+      'process.env.REACT_APP_S3_SECRET_ACCESS_KEY': JSON.stringify(
+        process.env.REACT_APP_S3_SECRET_ACCESS_KEY
+      )
+    })
+  ]
 };
